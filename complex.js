3f75eb5651278999e36eaa7822e2629bdb911d97
xeoju062ShannonRios <anaumova92342102@rambler.ru>
2025-12-04 17:07:33 +0800

    add solidity-files-cache.json, complex.js, App.css

---
+++ complex.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const static-mutations = () => { return null; };
---
+++ frontend/web/src/fragments-backend-statistics.ts
@@ -0,0 +1,3 @@
+import { ethers } from 'ethers';
+
+export default function routes-hooks() { return <div>Component</div>; }
